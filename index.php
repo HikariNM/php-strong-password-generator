12872e9f7e310165c49b50a809a46a3f1f62790e
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-04-13 21:38:14 +0200

    implement random password generation via GET request

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php 
+
+function paswGen(): string{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZO123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    $randPsw = '';
+    if (isset($_GET['psw'])){
+        $pswLeng = intval($_GET['psw']);
+        for ($i = 0; $i < $pswLeng; $i++){
+        $randPsw .= $chars[random_int(0, strlen($chars) - 1)];     
+        };
+        };
+        return $randPsw;
+};
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +23,16 @@
     <title>Document</title>
 </head>
 <body>
+
+<form action="" method="get">
+
+<label for="psw">Choose password length</label>
+<input type="number" name="psw">
+<button type="submit">Generate</button>
+
+</form>
     
+<?php echo paswGen() ?>
+
 </body>
 </html>
