1da1b413e8ac33d4398e2dc96c17c2a1e4226c41
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-04-13 21:41:06 +0200

    separate logic into functions.php and include it in index.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+function paswGen(): string{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZO123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    $randPsw = '';
+    if (isset($_GET['psw'])){
+        $pswLeng = intval($_GET['psw']);
+        for ($i = 0; $i < $pswLeng; $i++){
+        $randPsw .= $chars[random_int(0, strlen($chars) - 1)];     
+        };
+        };
+        return $randPsw;
+};
+
+
+?>
